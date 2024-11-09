b3a48e10025116dd96598de196366699064bbd39
jeus74kim <jwkim8936@naver.com>
2024-11-09 19:48:56 +0900

    test

---
+++ src/main/java/org/shopping/shopping_test_01/ShoppingTest01Application.java
@@ -0,0 +1,13 @@
+package org.shopping.shopping_test_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingTest01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingTest01Application.class, args);
+	}
+
+}
